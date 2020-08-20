eb7509867edb9c3561001d0c987a0dc728d2a78e
Hayden Faulds <fauldsh@gmail.com>
2020-08-20 12:02:13 +0100

    add runner-group creation and deletion

---
+++ github/actions_runner_groups.go
@@ -0,0 +1,67 @@
+// Copyright 2020 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"context"
+	"fmt"
+)
+
+// RunnerGroup represents a self-hosted runner group
+type RunnerGroup struct {
+	ID     *int64  `json:"id,omitempty"`
+	Name   *string `json:"name,omitempty"`
+	Visibility *string `json:"visibility,omitempty"`
+	Default *bool `json:"default,omitempty"`
+	RunnersURL *string `json:"runners_url,omitempty"`
+	SelectedRepositoriesURL *string `json:"selected_repositories_url,omitempty"`
+	Inherited *bool `json:"inherited,omitempty"`
+}
+
+type CreateRunnerGroupRequest struct {
+	Name   *string `json:"name,omitempty"`
+	Visibility *string `json:"visibility,omitempty"`
+	SelectedRepositoryIDs []string `json:"selected_repository_ids,omitempty"`
+	Runners []int64 `json:"runners"`
+}
+
+// CreateOrganizationRunnerGroup create a self-hosted runner group for an organization.
+// GitHub API docs: https://docs.github.com/en/rest/reference/actions#create-a-self-hosted-runner-group-for-an-organization
+func (s *ActionsService) CreateOrganizationRunnerGroup(ctx context.Context, org string, runnerGroupReq *CreateRunnerGroupRequest) (*RunnerGroup, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/runner-groups", org)
+
+	req, err := s.client.NewRequest("POST", u, runnerGroupReq)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	runnerGroup := &RunnerGroup{}
+	resp, err := s.client.Do(ctx, req, &runnerGroup)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return runnerGroup, resp, nil
+}
+
+// DeleteOrganizationRunnerGroup deletes a self-hosted runner group from an organization.
+// GitHub API docs: https://docs.github.com/en/rest/reference/actions#delete-a-self-hosted-runner-group-from-an-organization
+func (s *ActionsService) DeleteOrganizationRunnerGroup(ctx context.Context, org string, runnerGroupID int64) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/runner-groups/%d", org, runnerGroupID)
+
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	runnerGroup := &RunnerGroup{}
+	resp, err := s.client.Do(ctx, req, &runnerGroup)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
---
+++ github/actions_runner_groups_test.go
@@ -0,0 +1,79 @@
+// Copyright 2020 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestActionsService_CreateOrganizationRunnerGroup(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/runner-groups", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		fmt.Fprint(w, `{
+			  "id": 2,
+			  "name": "octo-runner-group",
+			  "visibility": "selected",
+			  "default": false,
+			  "selected_repositories_url": "https://api.github.com/orgs/octo-org/actions/runner-groups/2/repositories",
+			  "runners_url": "https://api.github.com/orgs/octo-org/actions/runner_groups/2/runners",
+			  "inherited": false
+			}`,
+		)
+	})
+
+	name := "octo-runner-group"
+	visibility := "selected"
+	req := &CreateRunnerGroupRequest{
+		Name:                  &name,
+		Visibility:            &visibility,
+		SelectedRepositoryIDs: []string{"MDEwOlJlcG9zaXRvcnkxMjk2MjY5"},
+		Runners:               []int64{2},
+	}
+	runnerGroup, _, err := client.Actions.CreateOrganizationRunnerGroup(context.Background(), "o", req)
+	if err != nil {
+		t.Errorf("Actions.CreateRegistrationToken returned error: %v", err)
+	}
+
+	id := int64(2)
+	selectedRepositoriesURL := "https://api.github.com/orgs/octo-org/actions/runner-groups/2/repositories"
+	runnersURL := "https://api.github.com/orgs/octo-org/actions/runner_groups/2/runners"
+	isDefault := false
+	inherited := false
+	want := &RunnerGroup{
+		ID: &id,
+		Name: &name,
+		Visibility: &visibility,
+		Default: &isDefault,
+		SelectedRepositoriesURL: &selectedRepositoriesURL,
+		RunnersURL: &runnersURL,
+		Inherited: &inherited,
+	}
+	if !reflect.DeepEqual(runnerGroup, want) {
+		t.Errorf("Actions.CreateOrganizationRunnerGroup returned %+v, want %+v", runnerGroup, want)
+	}
+}
+
+func TestActionsService_DeleteOrganizationRunnerGroup(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/runner-groups/2", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	_, err := client.Actions.DeleteOrganizationRunnerGroup(context.Background(), "o", 2)
+	if err != nil {
+		t.Errorf("Actions.DeleteOrganizationRunnerGroup returned error: %v", err)
+	}
+}
